638d31a0e8fe6e32304326c878b33c127c68d8fa
Andrew Nichols <andrew.nichols@gatech.edu>
2020-04-21 09:36:17 -0400

    Restructure directories and add a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# ZGit
+
+An exercise in learning both `zig` and more about the internals of `git`, roughly following [WYAG](https://wyag.thb.lt/) towards that end.
--- build.zig
+++ build.zig
@@ -1,7 +1,9 @@
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
-    const exe = b.addExecutable("zgit", "zgit.zig");
-    exe.setBuildMode(b.standardReleaseOptions());
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("zgit", "src/zgit.zig");
+    exe.setBuildMode(mode);
+    exe.setOutputDir("bin");
     b.default_step.dependOn(&exe.step);
 }
